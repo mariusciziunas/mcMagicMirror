ebbefef49828a780af9f54867983acc91012aef8
mariusciziunas <mr.marius.c@gmail.com>
2018-02-15 19:59:16 +0000

    feat: npm and webpack added

--- start.sh
+++ start.sh
@@ -1,7 +1,16 @@
 #!/bin/bash
 
+#wget https://nodejs.org/dist/v8.5.0/node-v8.5.0-linux-armv7l.tar.xz
+#tar -xvf node-v8.5.0-linux-armv7l.tar.xz
+#cd node-v8.5.0-linux-armv7l
+#cp -R * /usr/local/
+
+curl -sL https://deb.nodesource.com/setup_8.x | sudo -E bash -  # Install NodeJS v8
+sudo apt-get install -y nodejs  # npm nodejs-legacy #(Installed with nodesource)
+
 npm install
-./node_modules/.bin/webpack --config webpack.config.js --watch
+
+./node_modules/.bin/webpack --config webpack.config.js
 pip3 install -r requirements.txt
 python3 manage.py migrate
 python3 manage.py loaddata initial_data
